09e949977360cdd0ac3ad180ea3beda41b2c3470
kleyla <kleyla01@live.com>
2025-11-03 15:39:54 -0400

    docker mysql

---
+++ src/config/db.js
@@ -0,0 +1,21 @@
+// Get the client
+const mysql = require("mysql2/promise");
+const dotenv = require("dotenv");
+dotenv.config();
+
+// Create the connection to database
+const getConnection = async () => {
+  try {
+    return await mysql.createConnection({
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      port: process.env.DB_PORT,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
+    });
+  } catch (error) {
+    console.log("DB", error);
+  }
+};
+
+module.exports = getConnection;
